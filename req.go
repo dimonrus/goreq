dd38d6813b7517028f1b475f08c1ebb777971d9e
Дмитрий Солома <DISoloma@domclick.ru>
2018-09-19 16:33:16 +0300

    Log request errors

--- req.go
+++ req.go
@@ -177,6 +177,9 @@ func Ensure(request HttpRequest) (*http.Response, []byte, error) {
 	// Response
 	var response *http.Response
 
+	// Response body
+	var bodyBytes []byte
+
 	//Loop for retry count
 	for i := uint(0); i <= request.RetryCount; i++ {
 		//Get start time
@@ -187,19 +190,39 @@ func Ensure(request HttpRequest) (*http.Response, []byte, error) {
 		endTime := time.Now().UnixNano()
 		//Calc delta
 		delta = (endTime - startTime) / int64(time.Millisecond)
-		if response == nil || err != nil {
+		//If server does not respond
+		if err != nil {
 			//if no response than log
-			request.Logger.Print("\x1b[31;1m" + logCurl + "\n FAILED!!!\x1b[0m")
+			request.Logger.Printf("\x1b[31;1m" + logCurl + "\n %s \n FAILED!!!\x1b[0m", err)
 			if i >= request.RetryCount {
-				return nil, nil, &Error{Message: fmt.Sprintf("Http Request (%s) failed. Service: %s", request.Url, request.Label), HttpCode: http.StatusInternalServerError}
+				return nil, nil, &Error{Message: fmt.Sprintf("Http Request (%s) failed. Service: %s, Error: %s", request.Url, request.Label, err), HttpCode: http.StatusInternalServerError}
 			}
 			if request.RetryTimeout.Nanoseconds() > 0 {
 				time.Sleep(request.RetryTimeout)
 			}
 		} else {
+			// Read response
+			bodyBytes, err = ioutil.ReadAll(response.Body)
+			response.Body.Close()
+			if err != nil {
+				bodyBytes = []byte{}
+				logRequest(&request, response.StatusCode, &bodyBytes, delta, logCurl)
+				return nil, nil, &Error{
+					Message: fmt.Sprintf(
+						"Http Response (%s) read error: %s. Service: %s",
+						request.Host+request.Url,
+						err,
+						request.Label),
+					HttpCode: http.StatusInternalServerError,
+				}
+			}
+			//Log request
+			logRequest(&request, response.StatusCode, &bodyBytes, delta, logCurl)
+
 			//Check if can retry response
 			if canContinueRetry(response) {
-				response.Body.Close()
+				buffer := bytes.NewBuffer(request.Body)
+				req.Body = ioutil.NopCloser(buffer)
 				//Sleep before next round
 				if request.RetryTimeout.Nanoseconds() > 0 {
 					time.Sleep(request.RetryTimeout)
@@ -211,44 +234,31 @@ func Ensure(request HttpRequest) (*http.Response, []byte, error) {
 		}
 	}
 
-	//Close body
-	defer response.Body.Close()
+	return response, bodyBytes, err
+}
 
-	//Read response body
-	bodyBytes, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return nil, nil, &Error{
-			Message: fmt.Sprintf(
-				"Http Response (%s) read error: %s. Service: %s",
-				request.Host+request.Url,
-				err.Error(),
-				request.Label),
-			HttpCode: http.StatusInternalServerError,
-		}
-	}
-
+// Log request
+func logRequest(request *HttpRequest, responseStatus int, responseBody *[]byte, delta int64, curl string) {
 	//Log response status
-	logStatus := fmt.Sprintf("HTTP Status [%v] in: %v ms", response.StatusCode, delta)
+	logStatus := fmt.Sprintf("HTTP Status [%v] in: %v ms", responseStatus, delta)
 
 	//Log response body
 	var logBody string
-	if request.LogBodySize == 0 || len(bodyBytes) < request.LogBodySize {
-		logBody = fmt.Sprintf("Body: %s", strings.Join(strings.Fields(string(bodyBytes)), " "))
+	if request.LogBodySize == 0 || len(*responseBody) < request.LogBodySize {
+		logBody = fmt.Sprintf("Body: %s", strings.Join(strings.Fields(string(*responseBody)), " "))
 	} else {
-		logBody = fmt.Sprintf("Body: %s...", strings.Join(strings.Fields(string(bodyBytes)), " ")[:request.LogBodySize-1])
+		logBody = fmt.Sprintf("Body: %s...", strings.Join(strings.Fields(string(*responseBody)), " ")[:request.LogBodySize-1])
 	}
 
 	//If response status code more then 300 shows in red
-	if response.StatusCode >= 300 {
+	if responseStatus >= 300 {
 		logStatus = "\x1b[31;1m" + logStatus + "\x1b[0m"
 		logBody = "\x1b[31;1m" + logBody + "\x1b[0m"
 	} else {
 		logStatus = "\x1b[32;1m" + logStatus + "\x1b[0m"
 		logBody = "\x1b[32;1m" + logBody + "\x1b[0m"
 	}
-	request.Logger.Print("\n    ", "\x1b[34;1m"+logCurl+"\x1b[0m", "\n    ", logStatus, "\n    ", logBody)
-
-	return response, bodyBytes, err
+	request.Logger.Print("\n    ", "\x1b[34;1m"+curl+"\x1b[0m", "\n    ", logStatus, "\n    ", logBody)
 }
 
 // Ensure JSON request
@@ -299,7 +309,7 @@ func (r HttpRequest) EnsureJSON(method string, url string, header http.Header, b
 	if err != nil {
 		return response, err
 	}
-	
+
 	// Unmarshal response
 	if dto != nil {
 		err = json.Unmarshal(data, dto)
